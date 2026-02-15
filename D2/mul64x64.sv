9267b9defbdc7b31fbd665f49e979baa05663c09
isaacfallas <isaacfallas6@gmail.com>
2026-02-15 16:59:04 -0600

    Add pipeline stage to fix WNS

--- D2/mul64x64.sv
+++ D2/mul64x64.sv
@@ -6,6 +6,8 @@ module mul64x64(input logic clk,
   
 
   logic [15:0] p2 [0:7][0:7];
+  logic [15:0] p2_reg [0:7][0:7];
+  
   logic [127:0] p_shift2 [0:7][0:7];
   logic [127:0] sum64 [0:63];
   
@@ -17,7 +19,7 @@ module mul64x64(input logic clk,
                         .b(b[j*8 +: 8]),
                         .out(p2[i][j]));
         
-        shifter #(.SHIFT(i+j)) u_shifter(.product(p2[i][j]),
+        shifter #(.SHIFT(i+j)) u_shifter(.product(p2_reg[i][j]),
                                          .out(p_shift2[i][j]));
         
         assign sum64[i*8 + j]= p_shift2[i][j];
@@ -30,5 +32,21 @@ module mul64x64(input logic clk,
                        .rst_n(rst_n),
                        .sum64(sum64),
                        .result(out));
+                       
+  always_ff @(posedge clk or negedge rst_n) begin 
+    if (!rst_n) begin 
+      for (int i= 0; i< 8; i++) begin 
+        for (int j= 0; j< 8; j++) begin 
+          p2_reg[i][j] <= '0; 
+        end 
+      end 
+    end else begin 
+      for (int i= 0; i< 8; i++) begin 
+        for (int j= 0; j< 8; j++) begin 
+          p2_reg[i][j] <= p2[i][j]; 
+        end 
+      end 
+    end 
+  end
   
 endmodule
